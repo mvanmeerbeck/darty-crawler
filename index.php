12467d617ad907abc99bdd8eda8182e58548207b
Maxime.Vanmeerbeck <maxime.vanmeerbeck@gmail.com>
2018-01-29 21:32:01 +0100

    carac

--- index.php
+++ index.php
@@ -2,7 +2,34 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+$caracteristics = [
+    'Nombre de couverts',
+    'Format',
+    'Classe énergétique',
+    'Consommation d\'eau',
+    'Consommation d\'eau par cycle',
+    'Consommation d\'énergie',
+    'Niveau sonore (Norme EN 60704-3)',
+    'Nombre de programmes',
+    'Qualité de séchage',
+    'Sécurités',
+    'Hauteur',
+    'Largeur (cm)',
+    'Profondeur (cm)',
+    'Finition',
+    'Origine de fabrication',
+    'Disponibilité des pièces détachées',
+];
+
 $client = new \Goutte\Client();
+
+// Create and use a guzzle client instance that will time out after 90 seconds
+$guzzleClient = new \GuzzleHttp\Client([
+    'verify' => false,
+]);
+
+$client->setClient($guzzleClient);
+
 $crawler = $client->request('GET', 'https://www.darty.com/nav/extra/list?p=200&s=topa&cat=529');
 
 $links = $crawler->filter('div.infos_container > h2 > a');
@@ -11,16 +38,17 @@ $products = [];
 foreach ($links as $link) {
     echo 'https://www.darty.com' . $link->getAttribute('href') . PHP_EOL;
     $product = [
-        'url' => 'https://www.darty.com' . $link->getAttribute('href'),
-        'name' => $link->nodeValue
+        'Url' => 'https://www.darty.com' . $link->getAttribute('href'),
+        'Nom' => $link->nodeValue,
     ];
-    $productCrawler = $client->request('GET', $product['url']);
+    $productCrawler = $client->request('GET', $product['Url']);
 
     if (0 === $productCrawler->filter('meta[itemprop="price"]')->count()) {
         continue;
     }
 
-    $product['price'] = $productCrawler->filter('meta[itemprop="price"]')->attr('content');
+    $product['Prix'] = $productCrawler->filter('meta[itemprop="price"]')->attr('content');
+    $product['Marque'] = $productCrawler->filter('#darty_product_brand')->text();
 
     if ($productCrawler->filter('meta[itemprop="ratingValue"]')->count()) {
         $product['ratingValue'] = $productCrawler->filter('meta[itemprop="ratingValue"]')->attr('content');
@@ -31,7 +59,7 @@ foreach ($links as $link) {
     if ($productCrawler->filter('meta[itemprop="ratingCount"]')->count()) {
         $product['ratingCount'] = $productCrawler->filter('meta[itemprop="ratingCount"]')->attr('content');
     } else {
-        $product['ratingCount'] = null;
+        $product['ratingCount'] = 0;
     }
 
     $caracteristicNames = $productCrawler->filter('#product_caracteristics > div.product_bloc_content.bloc.ombre > table > tbody > tr > th')->each(function (\Symfony\Component\DomCrawler\Crawler $node, $i) {
@@ -41,12 +69,15 @@ foreach ($links as $link) {
         return $node->text();
     });
 
-    foreach ($caracteristicNames as $i => $caracteristicName) {
-        $product[$caracteristicName] = $caracteristicValues[$i];
+    foreach ($caracteristics as $caracteristic) {
+        if (false !== $key = array_search($caracteristic, $caracteristicNames)) {
+            $product[$caracteristic] = $caracteristicValues[$key];
+        } else {
+            $product[$caracteristic] = null;
+        }
     }
 
     $products[] = $product;
-
     sleep(1);
 }
 
